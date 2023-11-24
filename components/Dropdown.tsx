45d5c7cde77e0091cc29bd1a594145053b8734f2
Darseen <darseen.dev@gmail.com>
2023-11-25 02:02:17 +0300

    Initial commit

---
+++ components/Dropdown.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+// this is a replacement for the sidebar on mobile devices
+export default function Dropdown() {
+  return (
+    <div className="dropdown dropdown-bottom shadow-md">
+      <label tabIndex={0} className="btn btn-ghost m-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          className="inline-block w-5 h-5 stroke-current"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          ></path>
+        </svg>
+      </label>
+      <ul
+        tabIndex={0}
+        className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+      >
+        <li>
+          <a>Item 1</a>
+        </li>
+        <li>
+          <a>Item 2</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ components/Sidebar.tsx
@@ -0,0 +1,5 @@
+export default function Sidebar() {
+  return (
+    <div className="self-start w-64 hidden lg:block min-h-screen rounded-r-md bg-white shadow-md dark:bg-neutral border-r border-gray-300 dark:border-gray-600"></div>
+  );
+}
