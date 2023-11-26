5bd76f6e820ba61863886db7305ea5e58102b0f3
Darseen <darseen.dev@gmail.com>
2023-11-27 02:10:06 +0300

    Add tech section

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,29 +1,23 @@
-import getProducts from "@/lib/getProducts";
+import { Catagory } from "@/@types/types";
 import Image from "next/image";
 import React from "react";
 
-export default function Product() {
+export default function Product({ product }: { product: Catagory }) {
   return (
-    <section>
-      {getProducts("tech")?.map((product, i) => (
-        <div key={i} className="card w-96 bg-base-100 shadow-xl">
-          <figure>
-            <Image src={product.image} alt={product.name} />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">
-              Shoes!
-              <div className="badge badge-secondary">NEW</div>
-            </h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-              <div className="badge badge-outline">
-                {"$" + product.price + ".99"}
-              </div>
-            </div>
-          </div>
+    <div className="card sm:card-side bg-base-100 shadow-xl w-56 h-96 sm:w-auto sm:h-auto sm:max-w-sm rounded-lg hover:scale-105 transition-all duration-300 ease-in-out">
+      <figure className="!rounded-b-2xl sm:!rounded-2xl">
+        <Image src={product.image} alt={product.name} className="h-64 w-72" />
+      </figure>
+      <div className="card-body">
+        <div className="flex items-center justify-between">
+          <h2 className="card-title">{product.name}</h2>
+          <div className="badge badge-accent p-3">{"$" + product.price}</div>
         </div>
-      ))}
-    </section>
+        <p>Add this product to your cart now</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Add to Cart</button>
+        </div>
+      </div>
+    </div>
   );
 }
--- components/RecommendedProducts.tsx
+++ components/RecommendedProducts.tsx
@@ -1,10 +1,14 @@
-import React from "react";
 import Product from "./Product";
+import getProducts from "@/lib/getProducts";
 
 export default function RecommendedProducts() {
   return (
-    <section className="">
-      <Product />
+    <section className="flex overflow-x-auto p-4 w-full gap-4">
+      {getProducts("tech")?.map((product, i) => (
+        <div key={i} className="flex flex-nowrap">
+          <Product product={product} />
+        </div>
+      ))}
     </section>
   );
 }
